ef0575d26884c3cc0144bb0692ff3b73def5e4f8
sshlte55 <82154515+sshlte55@users.noreply.github.com>
2021-04-21 14:02:21 +0700

    Update and rename handlers/songs.py to penangan/songs.py

--- penangan/songs.py
+++ penangan/songs.py
@@ -22,7 +22,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 Finding the song...')
+    m = message.reply('🔎 Menemukan lagunya...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -41,17 +41,17 @@ def song(client, message):
 
     except Exception as e:
         m.edit(
-            "❌ Found Nothing.\n\nTry another keywork or maybe spell it properly."
+            "❌ Tidak ada yang ditemukan.\n\nTry another keywork or maybe spell it properly."
         )
         print(str(e))
         return
-    m.edit("Downloading the song by @Infinity_BOTs...")
+    m.edit("Downloading the song by @GB_03101999...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by @Infinity_BOTs**'
+        rep = '**🎵 Uploaded by @GB_03101999**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
